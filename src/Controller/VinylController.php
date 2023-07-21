a520702134ed4c77c46b888e968fa826ec916964
Tresor-ilunga <19im065@esisalama.org>
2023-07-21 12:53:45 +0200

    Added some Tiwggy goodness

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -19,7 +19,20 @@ final class VinylController extends AbstractController
     #[Route('/', name: 'vinyl_homepage', methods: ['GET'])]
     public function homepage(): Response
     {
-        return new Response('Hello World!');
+        $tracks =
+            [
+                ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+                ['song' => 'Waterfalls', 'artist' => 'TLC'],
+                ['song' => 'Creep', 'artist' => 'Radiohead'],
+                ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+                ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+                ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+            ];
+
+        return $this->render('pages/vinyl/index.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route('/browse/{slug}', name: 'vinyl_browse', methods: ['GET'])]
